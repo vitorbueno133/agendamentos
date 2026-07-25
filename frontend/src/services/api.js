8c4f20fcf3ba65705b689066727d84a466217077
Vitor Bueno <vitorbueno1333@gmail.com>
2026-07-25 18:44:47 -0300

    Aula 26

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000"; 
+const BASE_URL = import.meta.env.VITE_API_URL || 
+"http://localhost:5000"; 
   
 export async function buscarStatus() { 
   const resposta = await 
