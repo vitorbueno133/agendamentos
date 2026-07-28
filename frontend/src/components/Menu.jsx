a17f5f04c11b1731c41558558d197c5f1e6c0e18
Vitor Bueno <vitorbueno1333@gmail.com>
2026-07-28 15:40:46 -0300

    Aula 26.14

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,26 +1,37 @@
 function Menu({ telaAtiva, aoTrocarTela }) {
-  const telas = [ 
-    { chave: "agenda", rotulo: "Agenda" }, 
-    { chave: "clientes", rotulo: "Clientes" }, 
-    { chave: "profissionais", rotulo: "Profissionais"}, 
-    { chave: "servicos", rotulo: "Serviços" }, 
+  const telas = [
+    { chave: "agenda", rotulo: "Agenda" },
+    { chave: "clientes", rotulo: "Clientes" },
+    { chave: "profissionais", rotulo: "Profissionais" },
+    { chave: "servicos", rotulo: "Serviços" },
   ];
 
-  return ( 
-    <nav> 
-      {telas.map((tela) => ( 
-        <button 
-          key={tela.chave} 
-          onClick={() => aoTrocarTela(tela.chave)} 
-          style={{
-            fontWeight: telaAtiva === tela.chave ? "bold" : "normal", 
-          }} 
-        > 
-          {tela.rotulo} 
-        </button> 
-      ))} 
-    </nav> 
-  ); 
-} 
-  
+  return (
+    <header className="menu">
+      <div className="menu-logo">
+        <div className="logo">A</div>
+
+        <div>
+          <h2>Agendamentos SaaS</h2>
+          <span>Painel Administrativo</span>
+        </div>
+      </div>
+
+      <nav className="menu-nav">
+        {telas.map((tela) => (
+          <button
+            key={tela.chave}
+            className={`menu-btn ${
+              telaAtiva === tela.chave ? "ativo" : ""
+            }`}
+            onClick={() => aoTrocarTela(tela.chave)}
+          >
+            {tela.rotulo}
+          </button>
+        ))}
+      </nav>
+    </header>
+  );
+}
+
 export default Menu;
